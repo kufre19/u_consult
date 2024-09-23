ec0be234e5d59fec25affea5a4ea25e9590a08af
Emmanuel kufre <whitemaxwell5@gmail.com>
2024-09-23 14:23:15 +0100

    sal

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,3 +1,77 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Reset Password') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('password.update') }}">
+                            @csrf
+
+                            <input type="hidden" name="token" value="{{ $token }}">
+
+                            <div class="row mb-3">
+                                <label for="email"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="password"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="new-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="password-confirm"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control"
+                                        name="password_confirmation" required autocomplete="new-password">
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Reset Password') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
 
 <!doctype html>
 <html lang="en" data-bs-theme="blue-theme">
@@ -26,7 +100,8 @@
     <link
         href="{{ asset('assets/fonts.googleapis.com/css2ab59.css?family=Noto+Sans:wght@300;400;500;600&amp;display=swap') }}"
         rel="stylesheet">
-    <link href="{{ asset('assets/fonts.googleapis.com/cssf511.css?family=Material+Icons+Outlined') }}" rel="stylesheet">
+    <link href="{{ asset('assets/fonts.googleapis.com/cssf511.css?family=Material+Icons+Outlined') }}"
+        rel="stylesheet">
     <!--main css-->
     <link href="{{ asset('assets/css/bootstrap-extended.css') }}" rel="stylesheet">
     <link href="{{ asset('sass/main.css') }}" rel="stylesheet">
@@ -45,54 +120,71 @@
 
     <div class="section-authentication-cover">
         <div class="">
-          <div class="row g-0">
-    
-            <div class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex border-end bg-transparent">
-    
-              <div class="card rounded-0 mb-0 border-0 shadow-none bg-transparent bg-none">
-                <div class="card-body">
-                  <img src="{{ asset('assets/images/auth/forgot-password1.png')}}" class="img-fluid auth-img-cover-login" width="550"
-                    alt="">
-                </div>
-              </div>
-    
-            </div>
-    
-            <div class="col-12 col-xl-5 col-xxl-4 auth-cover-right align-items-center justify-content-center">
-              <div class="card rounded-0 m-3 mb-0 border-0 shadow-none bg-none">
-                <div class="card-body p-5">
-                  <img src="{{ asset('assets/images/logo1.png')}}" class="mb-4" width="145" alt="">
-                  <h4 class="fw-bold">Genrate New Password</h4>
-                  <p class="mb-0">We received your reset password request. Please enter your new password!</p>
-                  <div class="form-body mt-4">
-                    <form class="row g-3" method="POST" action="{{ route('password.update') }}" >
-                        @csrf
-                      <div class="col-12">
-                        <label class="form-label" for="NewPassword">New Password</label>
-                        <input type="text"  class="form-control" id="NewPassword" placeholder="Enter new password" name="password" required autocomplete="new-password">
-                      </div>
-                      <div class="col-12">
-                        <label class="form-label" for="ConfirmPassword">Confirm Password</label>
-                        <input type="text"  class="form-control" id="ConfirmPassword" placeholder="Confirm password" name="password_confirmation" required autocomplete="new-password">
-                      </div>
-                      <div class="col-12">
-                        <div class="d-grid gap-2">
-                          <button type="submit" class="btn btn-grd-branding">Change Password</button>
-                           <a href="{{ route('sign-in') }}" class="btn btn-grd-royal">Back to Login</a>
+            <div class="row g-0">
+
+                <div
+                    class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex border-end bg-transparent">
+
+                    <div class="card rounded-0 mb-0 border-0 shadow-none bg-transparent bg-none">
+                        <div class="card-body">
+                            <img src="{{ asset('assets/images/auth/forgot-password1.png') }}"
+                                class="img-fluid auth-img-cover-login" width="550" alt="">
                         </div>
-                      </div>
-                    </form>
-                  </div>
-        
-              </div>
-              </div>
+                    </div>
+
+                </div>
+
+                <div class="col-12 col-xl-5 col-xxl-4 auth-cover-right align-items-center justify-content-center">
+                    <div class="card rounded-0 m-3 mb-0 border-0 shadow-none bg-none">
+                        <div class="card-body p-5">
+                            <img src="{{ asset('assets/images/logo1.png') }}" class="mb-4" width="145"
+                                alt="">
+                            <h4 class="fw-bold">Genrate New Password</h4>
+                            <p class="mb-0">We received your reset password request. Please enter your new password!
+                            </p>
+                            <div class="form-body mt-4">
+                                <form class="row g-3" method="POST" action="{{ route('password.update') }}">
+                                    @csrf
+                                    <input type="hidden" name="token" value="{{ $token }}">
+
+
+                                    <div class="col-12">
+                                        <label class="form-label" for="email">Email Address</label>
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <label class="form-label" for="NewPassword">New Password</label>
+                                        <input type="password" class="form-control" id="NewPassword"
+                                            placeholder="Enter new password" name="password" required
+                                            autocomplete="new-password">
+                                    </div>
+                                    <div class="col-12">
+                                        <label class="form-label" for="ConfirmPassword">Confirm Password</label>
+                                        <input type="password" class="form-control" id="ConfirmPassword"
+                                            placeholder="Confirm password" name="password_confirmation" required
+                                            autocomplete="new-password">
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-grid gap-2">
+                                            <button type="submit" class="btn btn-grd-branding">Change
+                                                Password</button>
+                                            <a href="{{ route('sign-in') }}" class="btn btn-grd-royal">Back to
+                                                Login</a>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
-    
-          </div>
-          <!--end row-->
+            <!--end row-->
         </div>
-      </div>
-    
+    </div>
+
 
 
     <!--authentication-->
